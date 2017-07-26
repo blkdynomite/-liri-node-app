35393456c3209f6da64401e2f62fe2c13c27897a
Jared <jst9259@gmail.com>
2017-07-25 20:45:24 -0400

    version 2

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'uWInASG43KPH87fPOX77xIwFp',
+  consumer_secret: 'r6h2WI7T0QN5moGinmJjFWD6mYi4a4CgOPOSSQJXupBujGkcQf',
+  access_token_key: '879018994413576197-ylY8iGA3iG0Q9IPK7Cbatv7bzslLsfq',
+  access_token_secret: 'hffshoAljZvCKsBPUyNAD1RHvvchGmGJNAxOSyjpw18vP',
+}
---
+++ liri.js
@@ -0,0 +1,21 @@
+var keys = require('./keys.js');
+
+var Twitter = require('twitter');
+
+var getSpotifyThisSong = function();
+
+var client = new Twitter(keys.twitterKeys);
+
+// var client = new Twitter({
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// });
+ 
+var params = {screen_name: 'jstMAGA'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  }
+});
